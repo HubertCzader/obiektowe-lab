5641283d5f26e9b6b5c1d2f1024b4edeeea6f5cb
HubertCzader <hczader@student.agh.edu.pl>
2024-03-14 12:05:55 +0100

    Changed wrong config files

--- oolab/src/test/java/agh/ics/oop/OptionsParserTest.java
+++ oolab/src/test/java/agh/ics/oop/OptionsParserTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class OptionsParserTest {
+class
+OptionsParserTest {
     @Test
     public void testArrayToEnum(){
         String[] args = new String[]{"f", "b", "r", "l"};
